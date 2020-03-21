8d0d6619089039f425a1d059d6420307d138843e
Robby Gamble <robby.gamble2017@gmail.com>
2020-03-21 11:58:06 -0400

    I added code to prompt the user to enter their desired characters.

--- MIPS.s
+++ MIPS.s
@@ -1,11 +1,17 @@
 .data
 
 .text
-.global main
+.global main 
 .ent main
+message1: .asciiz "Input 20 characters max.\n" #I'll need this to communicate with the user
+
+
 
 main:
-
+#I'm about to ask the user to input characters
+li $v0, 4   	    #Command to print a string
+la $a0, message1    #Loading the string into the argument to enable printing
+syscall #executing command
 
 li $v0,10
 syscall
