24e412c17ab14bc44fca60866e70fd19335c6195
Robby Gamble <robby.gamble2017@gmail.com>
2020-03-21 12:47:40 -0400

    I added code to store the string input.

--- MIPS.s
+++ MIPS.s
@@ -4,6 +4,7 @@
 .global main 
 .ent main
 message1: .asciiz "Input 20 characters max.\n" #I'll need this to communicate with the user
+buffer: .space 64
 
 
 
@@ -12,6 +13,12 @@ main:
 li $v0, 4   	    #Command to print a string
 la $a0, message1    #Loading the string into the argument to enable printing
 syscall #executing command
+
+li $v0, 8 #Command to read a string
+la $a0, buffer #storing space for the string
+li $a1, 21 #allocating byte space for string to be stored
+syscall #executing command
+
 
 li $v0,10
 syscall
