f74c4e06bae72c5f90db57c939d4faceb6348b6e
Robby Gamble <robby.gamble2017@gmail.com>
2020-03-22 17:34:00 -0400

    I added code that I'll need to loop through my string.

--- MIPS.s
+++ MIPS.s
@@ -4,6 +4,8 @@
 .global main 
 .ent main
 message1: .asciiz "Input 20 characters max.\n" #I'll need this to communicate with the user
+message2: .asciiz "Output\n" #I'll display this after I finish converting everything
+message3: .asciiz "Invalid Input.\n" #I'll display this aft
 buffer: .space 64
 
 
@@ -18,6 +20,13 @@ li $v0, 8 #Command to read a string
 la $a0, buffer #storing space for the string
 li $a1, 21 #allocating byte space for string to be stored
 syscall #executing command
+
+addi $t0,$t0,0      #initializing my counter
+addi $t1,$t1,1      #trigger for loop 1: continues looping, 0: haults the loop
+addi $t2,$t2,0      #length of my string
+
+lengthOfString:
+
 
 
 li $v0,10
